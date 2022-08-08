10842d2a15cef671fe9fd09ab7eb3e707c399915
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-08 13:05:29 +0600

    double linked list, insert nth position node

--- linked_list/doubly_linked_list.cpp
+++ linked_list/doubly_linked_list.cpp
@@ -48,6 +48,57 @@ void insertBegin(node* &head, int value){
     head=n;
 }
 
+// insert nth position
+void insert_nth_position(node* &head,int value, int position){
+    node* n=new node(value);
+    int c=0;
+
+    if(position==0){
+        n->next=head;
+        head->previous=n;
+        head=n;
+        return;
+    }
+
+    node* temp=head;
+    while(temp->next!=NULL){
+        //c++; //add here if 1 based index
+        if(c==position){
+            temp->previous->next=n;
+            n->previous=temp->previous;
+            n->next=temp;
+            return;
+        }
+        c++; //add here if 0 based index
+        temp=temp->next;
+    }
+}
+
+// delete nth position node
+void delete_nth_node(node* &head, int pos){
+    // 1 based index
+    if(pos==1){
+        node* toDelete=head;
+        head=head->next;
+        head->next->previous=head;
+        delete toDelete;
+        return;
+    }
+
+    int c=0;
+    node* temp=head;
+    while(temp->next!=NULL){
+         c++;
+        if(c==pos){
+            temp->next->previous=temp->previous;
+            temp->previous->next=temp->next;
+            delete temp;
+            return;
+        }
+        temp=temp->next;
+    }
+}
+
 
 //print
 void print(node* &head){
@@ -80,6 +131,26 @@ int main(){
     print(head);
     cout<<"insert begin 7:" <<endl;
     insertBegin(head,7);
+    print(head);
+
+    //insert nth position
+    cout<<"insert 8 in 3rd position:" <<endl;
+    insert_nth_position(head,8,3);
+    print(head);
+
+    //insert 1st position
+    cout<<"insert 9 in 1st position:" <<endl;
+    insert_nth_position(head,9,1);
+    print(head);
+
+    // delete nth(1) position node
+    cout<<"delete 1 position node:" <<endl;
+    delete_nth_node(head,1);
+    print(head);
+
+    // delete nth(2) position node
+    cout<<"delete 3 position node:" <<endl;
+    delete_nth_node(head,3);
     print(head);
 
     return 0;
